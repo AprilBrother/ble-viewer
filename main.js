2db1beeb37c799dd639ec35c1a79d17213e0a6d2
volca <volca@tom.com>
2018-05-27 10:36:31 +0800

    ignore files

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+      width: 800, 
+      height: 600
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
